245b389bea6a4973f52225e6ec693f86d695c778
Moses Nderi <monderiee@gmail.com>
2014-08-30 19:52:18 +0300

    first commit

---
+++ _vti_cnf/index.php
@@ -0,0 +1,20 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|CeaserInc-PC\\CeaserInc
+vti_modifiedby:SR|CeaserInc-PC\\CeaserInc
+vti_timelastmodified:TR|25 Apr 2014 06:30:53 -0000
+vti_timecreated:TR|25 Apr 2014 06:28:06 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_structuredtm:TR|25 Apr 2014 06:28:16 -0000
+vti_nexttolasttimemodified:TW|25 Apr 2014 06:28:16 -0000
+vti_cacheddtm:TX|25 Apr 2014 06:30:53 -0000
+vti_filesize:IR|437
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|S|jquery/jquery.js
+vti_cachedsvcrellinks:VX|FSUS|jquery/jquery.js
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
+vti_charset:SR|utf-8
